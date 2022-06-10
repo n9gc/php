2604d44bc3d245e543c9f4e5f43a7045a5fab2f1
E0SelmY4V-PC <2507917746@qq.com>
2022-06-10 22:30:32 +0800

    add, refactor: 修改Rewrite为Url，添加域名函数

--- url.php
+++ url.php
@@ -3,15 +3,27 @@
 namespace ScpoPHP;
 
 /**
- * 重写url时更加方便
+ * url操作相关
  * @link http://scpo-php.seventop.top/rewrite/
  */
-class Rewrite {
+class Url
+{
+	/**
+	 * 获取域名
+	 * @return string 域名
+	 */
+	static public function dmn()
+	{
+		if (substr_count($dmn = $_SERVER['HTTP_HOST'], '.') === 2) $dmn = substr($dmn, strpos($dmn, '.') + 1);
+		if (($pos = strpos($dmn, ':')) !== false) $dmn = substr($dmn, 0, $pos);
+		return $dmn;
+	}
 	/**
 	 * 获取伪静态下访问的uri和get请求字符串
 	 * @return array 数组，分别为uri和get请求
 	 */
-	static public function uri_get() {
+	static public function rewrite_uriget()
+	{
 		if (($pos = strpos($uri = $_SERVER['QUERY_STRING'], '&')) !== false) {
 			$get = substr($uri, $pos);
 			$uri = substr($uri, 0, $pos);
