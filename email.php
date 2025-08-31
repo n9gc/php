2ce855c74d1d9681e297f4d814dac432f100cc79
E0SelmY4V <yabo1887415157@qq.com>
2025-08-31 19:33:18 +0800

    fix: 邮件功能不能正常发邮件

--- email.php
+++ email.php
@@ -3,7 +3,7 @@
 namespace ScpoPHP;
 
 require __DIR__ . '/lib/class.phpmailer.php';
-require __DIR__ .'/getconfig.php';
+require_once __DIR__ .'/getconfig.php';
 
 use ScpoPHP\Config\Email as Cfg;
 use ScpoPHP\Config\Base as BaseCfg;
@@ -30,6 +30,8 @@ class Email
 		$name = '',
 		$is_HTML = true
 	) {
+		$now = Cfg::$now;
+		$smtp = $now->smtp;
 		$mail = new \PHPMailer();
 		$mail->CharSet = BaseCfg::$now->charset;
 		if ($is_HTML) {
@@ -37,12 +39,12 @@ class Email
 			$mail->AltBody = '您的查看器不支持查看此HTML邮件！';
 		}
 		$mail->IsSMTP();
-		$mail->Host = Cfg::$smtp->host;
-		$mail->Port = Cfg::$smtp->port;
-		$mail->Username = Cfg::$smtp->name;
-		if ($mail->SMTPAuth = !empty(Cfg::$smtp->pwd)) $mail->Password = Cfg::$smtp->pwd;
-		$mail->From = Cfg::$now->addr;
-		$mail->FromName = Cfg::$now->name;
+		$mail->Host = $smtp->host;
+		$mail->Port = $smtp->port;
+		$mail->Username = $smtp->name;
+		if ($mail->SMTPAuth = !empty($smtp->pwd)) $mail->Password = $smtp->pwd;
+		$mail->From = $now->addr;
+		$mail->FromName = $now->name;
 		$mail->Subject = $subject;
 		$mail->Body = $body;
 		if (is_string($name)) $name = array($name);
